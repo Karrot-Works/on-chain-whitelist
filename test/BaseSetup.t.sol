c659eff8e4f74bba63bb4a2f70359104c11e55cd
Sweta Shaw <swt.shw94@gmail.com>
2024-04-10 16:48:46 +0530

    remove redundant check

--- test/BaseSetup.t.sol
+++ test/BaseSetup.t.sol
@@ -2,19 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {WhitelistNFT} from "../src/WhitelistNFT.sol";
+import {EarlyAccessNFT} from "../src/EarlyAccessNFT.sol";
 
 contract BaseSetup is Test {
-    WhitelistNFT whitelistNFTContract;
+    EarlyAccessNFT earlyAccessNFTContract;
     address public user1 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
     address public user2 = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
 
-    address public whitelistNFTAddress;
+    address public earlyAccessNFTAddress;
 
     function setUp() public virtual {
-        whitelistNFTContract = new WhitelistNFT("WhitelistNFT", "KARROT");
-        uint256 balance = whitelistNFTContract.balanceOf(user1);
-        whitelistNFTAddress = address(whitelistNFTContract);
-        assertEq(balance, 0);
+        earlyAccessNFTContract = new EarlyAccessNFT("EarlyAccessNFT", "KARROT");
+        earlyAccessNFTAddress = address(earlyAccessNFTContract);
     }
 }
